9a4967706e9fe48db544fdcfcd95ce5a8791a74b
Arix581 <113464283+Arix581@users.noreply.github.com>
2022-11-24 11:22:55 -0700

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,28 @@
+// Made by Benjamin Espenschied
+// V1.3
+
+using System;
+
+namespace myProgram
+{
+    class Grid
+    {
+        public int gridX;
+        public int gridY;
+        public string[,] gridData = new string[gridX, gridY];
+        
+        public Grid(X, Y)
+        {
+            gridX = X;
+            gridY = Y;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
